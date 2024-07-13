f3ed27ca965111365d8cc761b4e9e37d344aec82
fregnani123 <fabianofregnani@hotmail.com>
2024-07-13 10:53:08 -0300

    enviar

---
+++ src/actions.js
@@ -0,0 +1,15 @@
+
+//src/actions.js
+
+export const increment = () => {
+    return {
+        type: 'INCREMENT'
+    };
+};
+
+
+export const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    };
+};
