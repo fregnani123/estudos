75f696f230853fa28617224d3fb2ef396ccfdd05
fregnani123 <fabianofregnani@hotmail.com>
2024-07-13 15:21:02 -0300

    Redux

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 
 const initialState1 = { count: 0 };
 const initialState2 = { count: 0 };
+const initialState3 = { count: 0 };
 
 const counter1Reducer = (state = initialState1, action) => {
     if (action.payload !== 'counter1') return state;
@@ -26,7 +27,7 @@ const counter2Reducer = (state = initialState2, action) => {
             return state;
     }
 };
-const counter3Reducer = (state = initialState2, action) => {
+const counter3Reducer = (state = initialState3, action) => {
     if (action.payload !== 'counter2') return state;
     switch (action.type) {
         case 'INCREMENT':
@@ -40,5 +41,6 @@ const counter3Reducer = (state = initialState2, action) => {
 
 export default combineReducers({
     counter1: counter1Reducer,
-    counter2: counter2Reducer
+    counter2: counter2Reducer,
+    counter3: counter3Reducer
 });
